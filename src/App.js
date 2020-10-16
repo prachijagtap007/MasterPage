2d4573c757532d9ee2f4863ee5ae60736e9c6cc6
Prachi Jagtap <prachi.jagtap@cytel.com>
2020-10-16 21:05:00 +0530

    intial commit2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the plotly project
+          Welcome to the plotly project 2
         </p>
         <a
           className="App-link"
