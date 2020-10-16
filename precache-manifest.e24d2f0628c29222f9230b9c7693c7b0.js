fadc0f0e4b0cd3d7bc785bd6f5e86d7f075cbcec
Prachi Jagtap <prachi.jagtap@cytel.com>
2020-10-16 17:49:49 +0530

    Updates

---
+++ precache-manifest.e24d2f0628c29222f9230b9c7693c7b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f513c99ffa34a20ce6a4631e1fd46b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5e05bf6ce10e88a0b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73972dae8de30566f60f",
+    "url": "/static/js/2.88604fe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88604fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5e05bf6ce10e88a0b2",
+    "url": "/static/js/main.bfc729bf.chunk.js"
+  },
+  {
+    "revision": "f64772a02e3099bbd801",
+    "url": "/static/js/runtime-main.a1924312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
