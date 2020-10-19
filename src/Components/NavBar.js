5ebe0ba1918977416c6015e40db9b53eafc282bb
Prachi Jagtap <prachi.jagtap@cytel.com>
2020-10-20 00:17:07 +0530

    added Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,40 +1,63 @@
-import Nav from 'react-bootstrap/Nav'
-import React from 'react';
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useParams,
+  } from "react-router-dom";
+  import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+  import Home from './Home';
+  import AboutUs from './AboutUs';
+  import ContactUs from './ContactUs';
 
-export default function NavBar(){
+class Navbar extends React.Component{
 
-const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
-  return (
-    <div className="row">
-    <div className="col-md-12">
-    <Nav bg="dark" variant="dark" expand="lg" sticky="top" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-          NavLink 1 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-          NavLink 2 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" disabled>
-          NavLink 3 content
-        </Nav.Link>
-      </Nav.Item>
-      <NavDropdown title="Dropdown" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    </div>
-    </div>
-  );
-
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <Router>
+                            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mr-auto">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/about-us">Contact Us</Nav.Link>
+                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                    </NavDropdown>
+                                    </Nav>
+                                    <Form inline>
+                                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                    <Button variant="outline-success">Search</Button>
+                                    </Form>
+                                </Navbar.Collapse>
+                            </Navbar>
+                            <br />
+                            <Switch>
+                                <Route exact path="/">
+                                    <Home />
+                                </Route>
+                                <Route path="/about-us">
+                                    <AboutUs />
+                                </Route>
+                                <Route path="/contact-us">
+                                    <ContactUs />
+                                </Route>
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        )  
+    }
 }
+
+export default Navbar;
